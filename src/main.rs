37152c3dd4b0f7a0b2e6c863280168ee84f31e3f
Vinícius Melo <vinimelo@riseup.net>
2022-07-11 09:59:14 -0300

    Draws initial rectangle on the screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.13, 0.73, 0.13),
+            custom_size: Some(Vec2::new(1280.0, 720.0)),
+            ..default()
+        },
+        ..default()
+    });
 }
