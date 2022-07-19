94c5f2306c0b1ec547d93fe96401c979b05aa77e
Mai Porat <63251610+MaiTheLord@users.noreply.github.com>
2022-07-19 15:03:50 +0300

    improved numeric types in `board` (#7)

--- src/board.rs
+++ src/board.rs
@@ -22,22 +22,22 @@ fn bounds(mut query: Query<&mut Location, With<snake::Head>>) {
     }
 }
 
-pub type XCoord = i8;
-pub type YCoord = i8;
+pub type Coord = i8;
+pub type XCoord = Coord;
+pub type YCoord = Coord;
 
-pub const WIDTH: XCoord = 16;
-pub const HEIGHT: YCoord = 9;
+pub type Size = u8;
+pub type XSize = Size;
+pub type YSize = Size;
+
+pub const WIDTH: XSize = 16;
+pub const HEIGHT: YSize = 9;
 
 fn is_in_bounds(x: XCoord, y: YCoord) -> bool {
-    get_bound_range(WIDTH as i8).contains(&x) && get_bound_range(HEIGHT as i8).contains(&y)
+    get_bound_range(WIDTH).contains(&x) && get_bound_range(HEIGHT).contains(&y)
 }
 
-fn get_bound_range(size: i8) -> RangeInclusive<i8> {
-    let half = size as i8 / 2;
-
-    if size % 2 == 1 {
-        -half..=half
-    } else {
-        -(half - 1)..=half
-    }
+fn get_bound_range(size: Size) -> RangeInclusive<i8> {
+    let half = (size / 2) as i8;
+    if size % 2 == 1 { -half..=half } else { -(half - 1)..=half }
 }
